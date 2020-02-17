4d537e73870b277ebcdc2f0616e44d81d6425a83
Syvachenko Misha <misha.sivachenko@gmail.com>
2020-02-17 17:06:15 +0200

    Initial commit.

---
+++ ApplePickerPrototype/Assets/HighScore.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class HighScore : MonoBehaviour
+{
+    static public int score = 0;
+    void Awake()
+    { // 1 
+      // If the ApplePickerHighScore already exists, read it
+        if (PlayerPrefs.HasKey("ApplePickerHighScore"))
+        { // 2
+            score = PlayerPrefs.GetInt("ApplePickerHighScore");
+        }
+        // Assign the high score to ApplePickerHighScore
+        // 3
+        PlayerPrefs.SetInt("ApplePickerHighScore", score);
+    }
+    void Update()
+    {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+        // Update ApplePickerHighScore in PlayerPrefs if necessary
+        if (score > PlayerPrefs.GetInt("ApplePickerHighScore"))
+        { // 4
+            PlayerPrefs.SetInt("ApplePickerHighScore", score);
+        }
+    }
+}
